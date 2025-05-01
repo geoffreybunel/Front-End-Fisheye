97dad982d1e768094e21d1544a2a8e033b8bd17f
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-05-01 14:06:54 +0200

    Updated code to display photographers infos (added innerHTML in photographer.js)

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,31 +4,39 @@ function photographerTemplate(data) {
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
+        // const article = document.querySelector( 'article' );
+        // article.setAttribute("id", id);
+
+        // const img = document.getElementById( 'photographer__img' );
+        // img.setAttribute("src", picture);
+        
+        // const h2 = document.querySelector( 'h2' );
+        // h2.textContent = name;
+
+        // const localisation = document.querySelector( '.photographer__localisation' );
+        // localisation.textContent = city + ", " + country;
+
+        // const quote = document.querySelector( '.photographer__quote' );
+        // quote.textContent = tagline;
+
+        // const pricing = document.querySelector( '.photographer__pricing' );
+        // pricing.textContent = price + "€/jour";
+
+        //HTML IN JS
         const article = document.createElement( 'article' );
         article.setAttribute("id", id);
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
 
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const articleContent = `
+            <a href="#">
+                <img src="${picture}" alt="${name}">
+                <h2>${name}</h2>
+            </a>
+            <p class="photographer__localisation">${city}, ${country}</p>
+            <p class="photographer__quote">${tagline}</p>
+            <p class="photographer__pricing">${price}€/jour</p>
+        `;
 
-        const localisation = document.createElement( 'p' );
-        localisation.textContent = city + ", " + country;
-        localisation.classList.add('photographer__localisation');
-
-        const quote = document.createElement( 'p' );
-        quote.textContent = tagline;
-        quote.classList.add('photographer__quote');
-
-        const pricing = document.createElement( 'p' );
-        pricing.textContent = price + "€/jour";
-        pricing.classList.add('photographer__pricing');
-
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(localisation);
-        article.appendChild(quote);
-        article.appendChild(pricing);
+        article.innerHTML = articleContent;
         return (article);
     }
     return { name, picture, getUserCardDOM }
