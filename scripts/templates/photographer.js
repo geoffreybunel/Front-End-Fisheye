1ebbb1dbc6e7c05b006e9a7b274da4e1dcdb602a
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-05-01 11:39:51 +0200

    Import data from json file + added some css for photographer section on homepage

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("id", id);
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const localisation = document.createElement( 'p' );
+        localisation.textContent = city + ", " + country;
+        localisation.classList.add('photographer__localisation');
+
+        const quote = document.createElement( 'p' );
+        quote.textContent = tagline;
+        quote.classList.add('photographer__quote');
+
+        const pricing = document.createElement( 'p' );
+        pricing.textContent = price + "€/jour";
+        pricing.classList.add('photographer__pricing');
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(localisation);
+        article.appendChild(quote);
+        article.appendChild(pricing);
         return (article);
     }
     return { name, picture, getUserCardDOM }
