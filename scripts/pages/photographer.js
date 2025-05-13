4ac378bbfaaf14263d942e36ee688dd67d201ea2
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-05-13 16:40:48 +0200

    display medias (images)

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -21,7 +21,7 @@ function getPhotographerId() {
     return parseInt(urlParams.get("id"));
 }
 
-function displayPhotographerInfos(data) {
+function photographerInfos(data) {
     const { name, city, country, tagline } = data;
 
     const photographerTitleInfos = document.createElement("div");
@@ -34,15 +34,43 @@ function displayPhotographerInfos(data) {
     return photographerTitleInfos;
 }
 
-function displayPhotographerProfilePicture(data) {
-    const { portrait } = data;
+function photographerProfilePicture(data) {
+    const { portrait, name } = data;
 
     const photographerProfilePicture = document.createElement("div");
 
     photographerProfilePicture.innerHTML = `
-        <img class="profile_picture" src="../../assets/photographers/Photographers ID Photos/${portrait}" alt="">
+        <img class="profile_picture" src="../../assets/photographers/Photographers ID Photos/${portrait}" alt="${name}">
     `;
     return photographerProfilePicture;
+}
+
+function mediasFactory(media, photographerName) {
+    const { title, image, video } = media;
+    const photographerMediasContent = document.createElement("article");
+
+    if (image) {
+        photographerMediasContent.innerHTML = `
+            <img src="../../assets/photographers/${photographerName}/${image}" alt="${title}">
+            <p>${title}</p>
+        `;
+    } else if (video) {
+        photographerMediasContent.innerHTML =`
+            <img src="../../assets/photographers/${photographerName}/${video}" alt="${title}">
+            <p>${title}</p>
+        `;
+    }
+
+    return photographerMediasContent;
+}
+
+function displayMedias(media, photographerName) {
+    const photographerMediasContainer = document.querySelector(".photographer_medias");
+
+    media.forEach(mediaItem => {
+        const mediaModel = mediasFactory(mediaItem, photographerName);
+        photographerMediasContainer.append(mediaModel);
+    });
 }
 
 function displayData(photographers) {
@@ -51,8 +79,8 @@ function displayData(photographers) {
     const photographerId = getPhotographerId();
     const photographer = photographers.find(photographer => photographer.id === photographerId);
 
-    const photographerInfosModel = displayPhotographerInfos(photographer);
-    const photographerProfilePictureModel = displayPhotographerProfilePicture(photographer);
+    const photographerInfosModel = photographerInfos(photographer);
+    const photographerProfilePictureModel = photographerProfilePicture(photographer);
 
     photographerHeader.prepend(photographerInfosModel);
     photographerHeader.append(photographerProfilePictureModel);
@@ -60,112 +88,16 @@ function displayData(photographers) {
 
 async function init() {
     // Get data from photographers
-    const { photographers } = await getPhotographers();
+    const { photographers, media } = await getPhotographers();
+    const photographerId = getPhotographerId();
+
+    const photographer = photographers.find(p => p.id === photographerId);
+
     displayData(photographers);
+
+    const photographerMedias = media.filter(m => m.photographerId === photographerId);
+    
+    displayMedias(photographerMedias, photographer.name);
 }
 
 init();
-
-// // Function to create the page template
-// function photographerPageTemplate(data) {
-//     const { name, portrait, city, country, tagline } = data;
-//     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
-
-//     function getUserCardDOM() {
-//         // Create the photographer Title informations
-//         const photographTitleInfo = document.createElement( 'div' );
-
-//         // Photographer Title's HTML code
-//         const photographTitleInfoContent = `
-//             <h1 class="photographer__h1">${name}</h1>
-//             <p class="photographer__localisation">${city}, ${country}</p>
-//             <p class="photographer__quote">${tagline}</p>
-//         `;
-
-//         photographTitleInfo.innerHTML = photographTitleInfoContent;
-//         return photographTitleInfo;
-//     }
-    
-//     function getPhotographerProfilePicture() {
-//         // Create the photographer picture
-//         const photographImg = document.createElement( 'div' );
-
-//         // Photographer Title's HTML code
-//         const photographImgContent = `
-//             <img class="profile_picture" src="${picture}" alt="${name}">
-//         `;
-
-//         photographImg.innerHTML = photographImgContent;
-//         return photographImg;
-//     }
-
-//     return { getUserCardDOM, getPhotographerProfilePicture }
-// }
-
-// // Function to create the page template
-// function mediaTemplate(data, photographerName) {
-//     const { title, image } = data;
-
-//     function getMediaCardDOM() {
-//         // Create the photographer Title informations
-//         const article = document.createElement( 'article' );
-
-//         let firstName = photographerName.split(" ")[0];
-//         let mediaHTML = `<img src="../../assets/photographers/${firstName}/${image}" alt="${title}"`;
-
-//         const articleContent = `
-//             <div>
-//                 ${mediaHTML}
-//                 <p>${title}</p>
-//             </div>
-//         `;
-
-//         article.innerHTML = articleContent;
-//         return article;
-//     }
-
-//     return { getMediaCardDOM };
-// }
-
-// // Function to display Photographers's medias
-// async function displayMedia(media, photographerName) {
-//     const photographerSection = document.querySelector(".photographer_section");
-    
-//     // Create and append each media card
-//     media.forEach(mediaItem => {
-//         const mediaModel = mediaTemplate(mediaItem, photographerName);
-//         photographerSection.appendChild(mediaModel.getMediaCardDOM());
-//     });
-
-// }
-
-// // Function to display Photographers infos
-// async function displayData(photographers, media) {
-//     const photographerHeader = document.querySelector(".photograph-header");
-//     const contactButton = photographerHeader.querySelector(".contact_button");
-
-//     const photographerId = getPhotographerId();
-//     const photographer = photographers.find(photographer => photographer.id === photographerId);
-
-//     // // Filter media for this photographer
-//     // const photographerMedia = media.filter(item => item.photographerId === photographerId);
-    
-//     const photographerModel = photographerPageTemplate(photographer);
-
-//     photographerHeader.prepend(photographerModel.getUserCardDOM(), contactButton);
-//     photographerHeader.append(photographerModel.getPhotographerProfilePicture());
-
-//     // // Display Medias
-//     // displayMedia(photographerMedia, photographer.name)
-// }
-
-
-
-// // Function to initialize the code
-// async function init() {
-//     // Get data from photographers
-//     const { photographers } = await getPhotographers();
-//     displayData(photographers);
-// }
-
-// init();
