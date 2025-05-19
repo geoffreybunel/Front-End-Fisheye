639b639153ca818af4cc73640faa47f532e2bb04
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-05-19 13:34:08 +0100

    Contact modal finished

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -4,6 +4,7 @@ const modalContainer = document.querySelector(".contact_modal");
 const modal = document.querySelector(".modal");
 const modalOpenButton = document.querySelector(".contact_button");
 const modalCloseButton = document.querySelector(".modal_close_button");
+const form = document.getElementById("form");
 
 function displayModal() {
 	modalContainer.style.display = "block";
@@ -24,3 +25,21 @@ function closeModal() {
 
     modalOpenButton.focus();
 }
+
+// Prevent the modal from closing on Submit
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const firstName = document.getElementById("fname");
+    const lastName = document.getElementById("lname");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    console.log(firstName.value);
+    console.log(lastName.value);
+    console.log(email.value);
+    console.log(message.value);
+
+    closeModal();
+    form.reset();
+  })
